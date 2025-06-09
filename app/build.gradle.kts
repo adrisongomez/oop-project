6e42e71e01851421e807c5e7ca02b9e806400758
Adrison Gomez <74667594+adrisongomez@users.noreply.github.com>
2025-06-09 11:58:40 -0400

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,6 +26,7 @@ dependencies {
     implementation(libs.gson)
     implementation(libs.jbcrypt)
     implementation(libs.flyway.core)
+    implementation(libs.dotenv)
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.5.0")
