03775f1e8ed0982cb54980c3d8b9673996c9097d
Adrison Gomez <74667594+adrisongomez@users.noreply.github.com>
2025-06-20 06:39:45 -0400

    fix tests by providing in-memory repos

--- app/src/test/java/test/oopecommerce/api/HealthControllerTest.java
+++ app/src/test/java/test/oopecommerce/api/HealthControllerTest.java
@@ -7,11 +7,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @SpringBootTest(classes = com.oopecommerce.OopEcommerceApplication.class)
 @AutoConfigureMockMvc
+@Import(TestConfig.class)
 public class HealthControllerTest {
     @Autowired
     private MockMvc mockMvc;
